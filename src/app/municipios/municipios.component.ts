cd25576112388c2dba6b85dfda801f31d6141b0d
sbm-balmeida <beatriz.almeida@sbmtechnology.com.br>
2024-06-18 16:18:29 -0300

    Primeiro commit

---
+++ src/app/municipios/municipios.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { MunicipiosService } from '../municipios.service';
+
+@Component({
+  selector: 'app-municipios',
+  templateUrl: './municipios.component.html',
+  styleUrls: ['./municipios.component.css']
+})
+export class MunicipiosComponent implements OnInit {
+
+  municipios: any;
+  uf: string = '';
+
+  constructor(private municipiosService: MunicipiosService) { }
+
+  ngOnInit(): void {
+    this.getMunicipios();
+  }
+
+  getMunicipios(): void {
+    this.municipiosService.getMunicipios(this.uf).subscribe(data => {
+      this.municipios = data;
+    });
+  }
+
+}
